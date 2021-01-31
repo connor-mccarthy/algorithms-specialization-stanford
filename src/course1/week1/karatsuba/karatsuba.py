5e190d4a8080202f3530e51c62a30a60d2906856
Connor McCarthy <c.mccarthy@soseintel.com>
2021-01-31 14:45:20 -0700

    remove print statement

--- src/course1/week1/karatsuba/karatsuba.py
+++ src/course1/week1/karatsuba/karatsuba.py
@@ -27,11 +27,3 @@ def karatsuba(int1: int, int2: int) -> int:
     return (
         ((10 ** (2 * split_idx)) * ac) + ((10 ** (split_idx)) * ad_plus_bc) + bd
     )  # doesn't need to use karatsuba because will only improve lower order terms
-
-
-print(
-    karatsuba(
-        3141592653589793238462643383279502884197169399375105820974944592,
-        2718281828459045235360287471352662497757247093699959574966967627,
-    )
-)

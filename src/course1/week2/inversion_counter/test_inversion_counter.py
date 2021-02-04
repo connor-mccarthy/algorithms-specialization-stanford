c64e5ec5fd44e1f910836f79289c25bbb940a7ca
Connor McCarthy <c.mccarthy@soseintel.com>
2021-02-03 21:25:32 -0700

    update inversion counter tet

--- src/course1/week2/inversion_counter/test_inversion_counter.py
+++ src/course1/week2/inversion_counter/test_inversion_counter.py
@@ -1,6 +1,6 @@
 import pytest
 
-from inversion_counter import brute_force, sort_and_count
+from inversion_counter import brute_force, inversion_counter
 
 
 @pytest.mark.parametrize(
@@ -8,4 +8,4 @@ from inversion_counter import brute_force, sort_and_count
     [([1, 3, 5, 2, 4, 6]), ([8, 4, 2, 1]), ([8, 4, 2, 1, 500, 12, 80]), ([10]), ([])],
 )
 def test_count_inversions(array):
-    assert sort_and_count(array)[1] == brute_force(array)
+    assert inversion_counter(array)[1] == brute_force(array)

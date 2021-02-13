a1ced59bdbe806212e2678f6d416f958924f0b01
Connor McCarthy <c.mccarthy@soseintel.com>
2021-02-12 22:05:26 -0700

    update and correct quicksort

--- src/course1/week3/quicksort/quicksort.py
+++ src/course1/week3/quicksort/quicksort.py
@@ -5,32 +5,6 @@ from numpy import random
 # i = boundary between non-pivot elements we've looked at and those we haven't
 # j = boundary between elements less than pivot and those greater than pivot
 
-array = [3, 2, 8, 5, 1, 4, 7, 6]
-left = 0
-right = len(array)
-
-args = (array, left, right)
-
-
-def choose_pivot(array: List[int], left: int, right: int) -> int:
-    return random.randint(left, right)
-
-
-def make_pivot_first(array: List[int], left: int, i: int) -> None:
-    array[left], array[i] = array[i], array[left]
-
-
-def partition(array: List[int], left: int, right: int) -> int:
-    i = left + 1
-    for j in range(left + 1, right):
-        if array[j] < array[left]:
-            array[i], array[j] = array[j], array[i]
-            i += 1
-
-    array[left], array[i - 1] = array[i - 1], array[left]
-
-    return i - 1
-
 
 def quicksort(
     array: List[int], left: Optional[int] = None, right: Optional[int] = None
@@ -40,10 +14,29 @@ def quicksort(
         return array
 
     left = 0 if left is None else left
-    right = len(array) if right is None else right
+    right = len(array) - 1 if right is None else right
 
     if left < right:
-        new_pivot = partition(array, left, right)
+        pivot = choose_pivot(array, left, right)
+        array[left], array[pivot] = array[pivot], array[left]
+        split_marker = partition(array, left, right)
 
-        quicksort(array, left, new_pivot)
-        quicksort(array, new_pivot + 1, right)
+        quicksort(array, left, split_marker)
+        quicksort(array, split_marker + 1, right)
+
+
+def choose_pivot(array: List[int], left: int, right: int) -> int:
+    return random.randint(left, right)
+
+
+def partition(array: List[int], left: int, right: int) -> int:
+    pivot_element = array[left]
+    i = left + 1
+    for j in range(left + 1, right + 1):
+        if array[j] < pivot_element:
+            array[i], array[j] = array[j], array[i]
+            i += 1
+
+    array[left], array[i - 1] = array[i - 1], array[left]
+
+    return i - 1

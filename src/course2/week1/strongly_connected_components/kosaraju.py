6e0b3b34a55a18fba80aa04a57eb9c20c727e865
Connor McCarthy <c.mccarthy@soseintel.com>
2021-02-24 16:01:29 -0700

    update kosaraju

--- src/course2/week1/strongly_connected_components/kosaraju.py
+++ src/course2/week1/strongly_connected_components/kosaraju.py
@@ -1,53 +1,54 @@
-from typing import Any, Dict, List, Optional
+from typing import Dict, List, Optional
 
-Graph = Dict[int, List[int]]
-TrackedGraph = Dict[int, Dict[str, Any]]
+Graph = List[List[int]]
 
 
 class Kosaraju:
-    def __init__(self, graph: Graph) -> None:
-        self.graph = self.reverse_graph(graph)
-        self.t = 0
+    def __init__(self, adj_list: List[List[int]]) -> None:
+        self.num_nodes = max(node for row in adj_list for node in row)
+        self.get_graphs(adj_list)
+        self.t = len(self.rev_graph)
         self.start_node: Optional[int] = None
-        self.explored_nodes: List[int] = []
+        self.visited = [False] * (self.num_nodes + 1)
         self.finishing_times: List[int] = []
         self.sccs: Dict[int, int] = {}
 
-    def reverse_graph(self, graph: Graph) -> Graph:
-        reversed_graph: Graph = {node: [] for node in graph}
-        for node, adjacent_nodes in graph.items():
-            for adjacent_node in adjacent_nodes:
-                reversed_graph[adjacent_node].append(node)
-        return reversed_graph
+    def get_graphs(self, adj_list: List[List[int]]) -> None:
+        graph: Graph = [[] for _ in range(self.num_nodes + 1)]
+        rev_graph: Graph = [[] for _ in range(self.num_nodes + 1)]
+        for line in adj_list:
+            graph[line[0]] += [line[1]]
+            rev_graph[line[1]] += [line[0]]
+        self.graph = graph
+        self.rev_graph = rev_graph
 
     def first_pass(self) -> List[int]:
-        self.current_label = len(self.graph)
-        for vertex in self.graph:
-            if vertex not in self.explored_nodes:
+        for vertex in range(self.num_nodes):
+            if not self.visited[vertex]:
                 self.start_node = vertex
                 self.dfs(vertex)
         self.finishing_times.reverse()
 
     def dfs(self, start_node: int = 0) -> None:
-        self.explored_nodes.append(start_node)
-        for adjacent_node in self.graph[start_node]:
-            if adjacent_node not in self.explored_nodes:
+        self.visited[start_node] = True
+        for adjacent_node in self.rev_graph[start_node]:
+            if not self.visited[adjacent_node]:
                 self.dfs(adjacent_node)
         self.finishing_times.append(start_node)
-        self.current_label -= 1
+        self.t -= 1
 
     def second_pass(self) -> None:
         self.num_scc = 0
         for node in self.finishing_times:
-            if node not in self.explored_nodes:
+            if not self.visited[node]:
                 self.num_scc += 1
                 self.dfs_scc(node)
 
     def dfs_scc(self, start_node: int):
-        self.explored_nodes.append(start_node)
+        self.visited[start_node] = True
         self.sccs[start_node] = self.num_scc
         for adjacent_node in self.graph[start_node]:
-            if adjacent_node not in self.explored_nodes:
+            if not self.visited[adjacent_node]:
                 self.dfs_scc(adjacent_node)
 
     def aggregate_scc(self) -> Dict[int, List[int]]:
@@ -61,25 +62,25 @@ class Kosaraju:
 
     def run(self) -> Dict[int, List[int]]:
         self.first_pass()
-        self.t = 0
         self.start_node = None
-        self.explored_nodes = []
-        self.graph = self.reverse_graph(self.graph)
+        self.visited = [False] * (self.num_nodes + 1)
         self.second_pass()
         return self.aggregate_scc()
 
 
-graph = {
-    1: [7],
-    2: [5],
-    3: [9],
-    4: [1],
-    5: [8],
-    6: [3, 8],
-    7: [4, 9],
-    8: [2],
-    9: [6],
-}
+# graph = {
+#     1: [7],
+#     2: [5],
+#     3: [9],
+#     4: [1],
+#     5: [8],
+#     6: [3, 8],
+#     7: [4, 9],
+#     8: [2],
+#     9: [6],
+# }
+# graph = [[1, 7], [2, 5], [3, 9], [4, 1], [5, 8], [6, 3, 8], [7, 4, 9], [8, 2], [9, 6]]
+graph = [[0, 6], [1, 4], [2, 8], [3, 0], [4, 7], [5, 2, 7], [6, 3, 8], [7, 1], [8, 5]]
 kosaraju = Kosaraju(graph)
 actual = kosaraju.run()
 print(actual)
--- src/course2/week1/strongly_connected_components/test_kosaraju.py
+++ src/course2/week1/strongly_connected_components/test_kosaraju.py
@@ -1,22 +1,27 @@
 from kosaraju import Kosaraju
 
 
-def test_kosaru():
-    graph = {
-        0: [6],
-        1: [4],
-        2: [8],
-        3: [0],
-        4: [7],
-        5: [2, 7],
-        6: [3, 8],
-        7: [1],
-        8: [5],
-    }
-    kosaraju = Kosaraju(graph)
+def test_kosaraju():
+    adj_list = [
+        [0, 6],
+        [1, 4],
+        [2, 8],
+        [3, 0],
+        [4, 7],
+        [5, 2, 7],
+        [6, 3, 8],
+        [7, 1],
+        [8, 5],
+    ]
+    kosaraju = Kosaraju(adj_list)
     actual = kosaraju.run()
-    expected = {1: [1, 4, 7], 2: [2, 5, 8], 3: [0, 3, 6]}
-    print(actual)
-    for scc_id, nodes in actual.items():
-        for node in nodes:
-            assert node in expected[scc_id]
+    expected = [[1, 4, 7], [2, 5, 8], [0, 3, 6]]
+    for actual_component in actual.values():
+        component_found = False
+        for expected_component in expected:
+            if set(expected_component) == set(actual_component):
+                component_found = True
+        assert component_found
+    # for scc_id, nodes in actual.items():
+    #     for node in nodes:
+    #         assert node in expected[scc_id]

0de8c5cc9670eb6a32631c020a2ca80dc82a47ba
Connor McCarthy <c.mccarthy@soseintel.com>
2021-02-24 00:00:48 -0700

    working kosuraju

--- src/course2/week1/strongly_connected_components/kosaraju.py
+++ src/course2/week1/strongly_connected_components/kosaraju.py
@@ -1,80 +1,90 @@
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 
 Graph = Dict[int, List[int]]
 TrackedGraph = Dict[int, Dict[str, Any]]
-SCCDict = Dict[int, List[int]]
 
 
 class Kosaraju:
     def __init__(self, graph: Graph) -> None:
-        self.graph = graph
-        self.grev = self.make_tracker(self.reverse_graph)
-        self.g = self.make_tracker(self.graph)
+        self.g = graph
+        self.grev = self.reverse_graph(self.g)
+        self.t = 0
+        self.start_node: Optional[int] = None
+        self.explored_nodes: List[int] = []
+        self.finishing_times: Dict[int, int] = {}
+        self.sccs: Dict[int, int] = {}
 
-    @property
-    def reverse_graph(self) -> Graph:
-        reversed_graph: Graph = {node: [] for node in self.graph}
-        for node, adjacent_nodes in self.graph.items():
+    def reverse_graph(self, graph: Graph) -> Graph:
+        reversed_graph: Graph = {node: [] for node in graph}
+        for node, adjacent_nodes in graph.items():
             for adjacent_node in adjacent_nodes:
                 reversed_graph[adjacent_node].append(node)
         return reversed_graph
 
-    def make_tracker(self, graph: Graph) -> TrackedGraph:
-        return {
-            node: {
-                "nodes": graph[node],
-                "is_explored": False,
-                "finish_time": None,
-                "leader": None,
-                "scc": None,
-            }
-            for node in graph
-        }
-
     def first_pass(self) -> List[int]:
-        self.t = len(self.grev)
-        self.start_node = None
-        for vertex in reversed(range(len(self.grev))):
-            if not self.grev[vertex]["is_explored"]:
+        self.current_label = len(self.grev)
+        for vertex in self.grev:
+            if vertex not in self.explored_nodes:
                 self.start_node = vertex
                 self.dfs(vertex)
-        sorted_tuples = sorted(self.grev.items(), key=lambda x: x[1]["finish_time"])
-        self.grev = {node: info for node, info in sorted_tuples}
+        self.grev = {
+            k: v
+            for k, v in sorted(
+                self.grev.items(), key=lambda x: self.finishing_times[x[0]]
+            )
+        }
 
     def dfs(self, start_node: int = 0) -> None:
-        self.grev[start_node]["is_explored"] = True
-        self.grev[start_node]["leader"] = self.start_node
-        for adjacent_node in self.grev[start_node]["nodes"]:
-            if not self.grev[adjacent_node]["is_explored"]:
+        self.explored_nodes.append(start_node)
+        for adjacent_node in self.grev[start_node]:
+            if adjacent_node not in self.explored_nodes:
                 self.dfs(adjacent_node)
-        self.grev[start_node]["finish_time"] = self.t
-        self.t -= 1
+        self.finishing_times[start_node] = self.current_label
+        self.current_label -= 1
 
-    def second_pass(self) -> SCCDict:
+    def second_pass(self) -> None:
         self.num_scc = 0
         for node in self.grev:
-            if not self.g[node]["is_explored"]:
+            if node not in self.explored_nodes:
                 self.num_scc += 1
                 self.dfs_scc(node)
-        return self.aggregate_scc(self.g)
 
     def dfs_scc(self, start_node: int):
-        self.g[start_node]["is_explored"] = True
-        self.g[start_node]["scc"] = self.num_scc
-        for adjacent_node in self.g[start_node]["nodes"]:
-            if not self.g[adjacent_node]["is_explored"]:
+        self.explored_nodes.append(start_node)
+        self.sccs[start_node] = self.num_scc
+        for adjacent_node in self.reverse_graph(self.grev)[start_node]:
+            if adjacent_node not in self.explored_nodes:
                 self.dfs_scc(adjacent_node)
 
-    def aggregate_scc(self, graph_tracker: TrackedGraph) -> SCCDict:
-        scc_dict: SCCDict = {}
-        for k, v in self.g.items():
-            scc = v["scc"]
-            if scc in scc_dict:
-                scc_dict[scc].append(k)
+    def aggregate_scc(self) -> Dict[int, List[int]]:
+        grouped_sccs: Dict[int, List[int]] = {}
+        for node, group in self.sccs.items():
+            if group in grouped_sccs:
+                grouped_sccs[group].append(node)
             else:
-                scc_dict[scc] = [k]
-        return scc_dict
+                grouped_sccs[group] = [node]
+        return grouped_sccs
 
     def run(self) -> Dict[int, List[int]]:
         self.first_pass()
-        return self.second_pass()
+        self.t = 0
+        self.start_node = None
+        self.explored_nodes = []
+        self.second_pass()
+        return self.aggregate_scc()
+
+
+graph = {
+    1: [7],
+    2: [5],
+    3: [9],
+    4: [1],
+    5: [8],
+    6: [3, 8],
+    7: [4, 9],
+    8: [2],
+    9: [6],
+}
+kosaraju = Kosaraju(graph)
+actual = kosaraju.run()
+print(actual)

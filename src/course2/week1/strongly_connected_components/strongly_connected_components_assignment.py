d85d967be33171f3787fbc182527097ed9b6143b
Connor McCarthy <c.mccarthy@soseintel.com>
2021-02-24 17:01:00 -0700

    clean up

--- src/course2/week1/strongly_connected_components/strongly_connected_components_assignment.py
+++ src/course2/week1/strongly_connected_components/strongly_connected_components_assignment.py
@@ -2,7 +2,6 @@ import faulthandler
 import os
 import resource
 import sys
-import threading
 from typing import Dict, List
 
 from kosaraju import Kosaraju
@@ -40,48 +39,6 @@ def get_largest_component_sizes(sccs: Dict[int, List[int]], components: int) -> 
     return output
 
 
-def get_small_data():
-    lines = [
-        [1, 4],
-        [2, 8],
-        [3, 6],
-        [4, 7],
-        [5, 2],
-        [6, 9],
-        [7, 1],
-        [8, 5],
-        [8, 6],
-        [9, 7],
-        [9, 3],
-    ]
-    return [[node - 1 for node in line] for line in lines]
-
-
-def also_get_small_data():
-    return [
-        [0, 1],
-        [1, 2],
-        [1, 3],
-        [1, 4],
-        [2, 5],
-        [3, 4],
-        [3, 6],
-        [4, 1],
-        [4, 5],
-        [4, 6],
-        [5, 2],
-        [5, 7],
-        [6, 7],
-        [6, 9],
-        [7, 6],
-        [8, 6],
-        [9, 8],
-        [9, 10],
-        [10, 11],
-        [11, 9],
-    ]
-
-
 def main() -> str:
     lines = get_data()
     kosaraju = Kosaraju(lines)
@@ -89,13 +46,6 @@ def main() -> str:
     components = 5
     return get_largest_component_sizes(sccs, components)
 
-
-sys.setrecursionlimit(2097152)  # adjust numbers
-threading.stack_size(134217728)  # for your needs
-
-main_thread = threading.Thread(target=main)
-main_thread.start()
-main_thread.join()
 
 if __name__ == "__main__":  # pragma: no cover
     print(main())
--- src/course2/week1/strongly_connected_components/test_kosaraju.py
+++ src/course2/week1/strongly_connected_components/test_kosaraju.py
@@ -1,27 +1,53 @@
+import pytest
+
 from kosaraju import Kosaraju
 
+adj_list1 = [
+    [0, 6],
+    [1, 4],
+    [2, 8],
+    [3, 0],
+    [4, 7],
+    [5, 2, 7],
+    [6, 3, 8],
+    [7, 1],
+    [8, 5],
+]
+expected1 = [[1, 4, 7], [2, 5, 8], [0, 3, 6]]
+adj_list2 = [
+    [0, 1],
+    [1, 2],
+    [1, 3],
+    [1, 4],
+    [2, 5],
+    [3, 4],
+    [3, 6],
+    [4, 1],
+    [4, 5],
+    [4, 6],
+    [5, 2],
+    [5, 7],
+    [6, 7],
+    [6, 9],
+    [7, 6],
+    [8, 6],
+    [9, 8],
+    [9, 10],
+    [10, 11],
+    [11, 9],
+]
+expected2 = [[6, 7, 9, 8, 10, 11], [2, 5], [1, 3, 4], [0]]
 
-def test_kosaraju():
-    adj_list = [
-        [0, 6],
-        [1, 4],
-        [2, 8],
-        [3, 0],
-        [4, 7],
-        [5, 2, 7],
-        [6, 3, 8],
-        [7, 1],
-        [8, 5],
-    ]
+
+@pytest.mark.parametrize(
+    "adj_list, expected", [(adj_list1, expected1), (adj_list2, expected2)]
+)
+def test_kosaraju(adj_list, expected):
     kosaraju = Kosaraju(adj_list)
     actual = kosaraju.run()
-    expected = [[1, 4, 7], [2, 5, 8], [0, 3, 6]]
     for actual_component in actual.values():
-        component_found = False
-        for expected_component in expected:
-            if set(expected_component) == set(actual_component):
-                component_found = True
+        component_found = any(
+            set(actual_component) == set(expected_component)
+            for expected_component in expected
+        )
         assert component_found
-    # for scc_id, nodes in actual.items():
-    #     for node in nodes:
-    #         assert node in expected[scc_id]

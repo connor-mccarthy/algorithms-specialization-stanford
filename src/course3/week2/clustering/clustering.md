0b86d8e977b2c2e3c1df2c0b34d2dc15810bedd3
Connor McCarthy <c.mccarthy@soseintel.com>
2021-03-29 22:00:22 -0600

    fix clustering setup

--- src/course3/week2/clustering/clustering.md
+++
@@ -1,8 +0,0 @@
-from clustering import clustering
-
-
-def test_clustering():
-    input_data = None
-    actual = kruskal(input_data)
-    expcted = None
-    assert actual == expected
---
+++ src/course3/week2/clustering/test_clustering.py
@@ -0,0 +1,8 @@
+from clustering import clustering
+
+
+def test_clustering():
+    input_data = None
+    actual = clustering(input_data)
+    expected = None
+    assert actual == expected
